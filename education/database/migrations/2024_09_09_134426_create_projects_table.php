503b9f02e2f710d059bf084bd6ee71d2fd2b54eb
Bito-Zalan <bitozalan23@gmail.com>
2024-09-10 16:05:18 +0200

    project tabla letrehozva

---
+++ education/database/migrations/2024_09_09_134426_create_projects_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id()->references('project_id')->on('tasks');
+            $table->string('name');
+            $table->integer('cost');
+            $table->integer('time')->default(0);
+            $table->foreignId('manager_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
